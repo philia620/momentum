88cce490299c9aa3ec245b6aa92fc98e008b5606
JihunChoi <geehoon62017@gmail.com>
2020-03-23 18:06:21 +0900

    complete background

---
+++ background.js
@@ -0,0 +1,23 @@
+const body = document.querySelector("body");
+
+const IMG_COUNT = 3;
+
+
+function paintImage(imgNumber) {
+    const image = new Image();
+    image.src = `images/${imgNumber + 1}.jpg`;
+    image.classList.add("bgImage");
+    body.appendChild(image);
+}
+
+function getRandomNumber() {
+    const number = Math.floor(Math.random() * IMG_COUNT);
+    return number;
+}
+
+function init() {
+    const randomNumber = getRandomNumber();
+    paintImage(randomNumber);
+}
+
+init();
