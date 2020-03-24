8e403cd8ca574cd70af5ed2bd96395829be7cf5b
JihunChoi <geehoon62017@gmail.com>
2020-03-24 13:36:55 +0900

    back finish

--- weather.js
+++ weather.js
@@ -1,7 +1,22 @@
+const weather = document.querySelector(".js-weather");
+
+const API_KEY = "1bed7402c5790b279e0eff171bd1067f";
 const COORDS = "coords";
 
+function getWeather(latitude, longitude) {
+    fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
+    ).then(function(response) {
+        return response.json()
+    }).then(function(json) {
+        const temperature = json.main.temp;
+        const place = json.name;
+        weather.innerHTML = `${temperature} @ ${place}`;
+    })
+}
+
 function saveCoords(coordsObj) {
-    localStorage.setItem(COORDS, JSON.stringify(COORDS));
+    localStorage.setItem(COORDS, JSON.stringify(coordsObj));
 }
 
 function handleGeoSuccess(position) {
@@ -12,6 +27,7 @@ function handleGeoSuccess(position) {
         longitude: longitude
     };
     saveCoords(coordsObj);
+    getWeather(latitude, longitude);
 }
 
 function handleGeoError() {
@@ -23,11 +39,13 @@ function askForCoords() {
 }
 
 function loadCoords() {
-    const loadedCords = localStorage.getItem(COORDS);
-    if (loadedCords === null) {
+    const loadedCoords = localStorage.getItem(COORDS);
+    if (loadedCoords === null) {
         askForCoords();
     } else {
-        getWeather();
+        const paresedCoords = JSON.parse(loadedCoords);
+        console.log(paresedCoords);
+        getWeather(paresedCoords.latitude, paresedCoords.longitude);
     }
 }
 
